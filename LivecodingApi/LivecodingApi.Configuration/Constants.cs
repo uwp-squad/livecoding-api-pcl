972f99af39e752823564ad73d28147aec00c180b
Odonno <bottiau.david@laposte.net>
2016-08-21 21:12:20 +0200

    feat(configuration): create constants of the api

---
+++ LivecodingApi/LivecodingApi.Configuration/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivecodingApi.Configuration
+{
+    public class Constants
+    {
+        /// <summary>
+        /// Base URL of the livecoding API
+        /// </summary>
+        public static string ApiBaseUrl = "https://www.livecoding.tv:443/api/";
+
+        /// <summary>
+        /// Version of the current livecoding API
+        /// </summary>
+        public static string ApiVersion = "v1/";
+    }
+}
